8e137ce9ec82b5ab1dafa16cef7e4315b0a0f47b
Saad Aulakh <saadaulakh04@gmail.com>
2023-11-29 10:25:19 -0500

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,6 @@ export default authMiddleware({
 });
  
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)', '/((?!ws$).*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
 };
  
